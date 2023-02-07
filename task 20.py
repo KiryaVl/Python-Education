c1c02dbd385eebbbebe413a3ec8c04d2a2fe37a9
Kirya <rabbitnya@gmail.com>
2023-02-07 19:45:08 +0300

    Added in 20 task

--- task 20.py
+++ task 20.py
@@ -21,8 +21,8 @@ def isCyrillic(text):
 	return bool(re.search('[а-яА-Я]', text))
 eng = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP' , 4: 'FHVWY', 5: 'K', 8: 'JX', 10: 'QZ'}
 rus = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ', 4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
-text = input().upper()
+text = input("Введите слово: ").upper()
 if isCyrillic(text):
-	print(sum([k for i in text for k, v in rus.items() if i in v]))
+	print(sum([k for i in text for k, v in rus.items() if i in v]), " очков")
 else:
-	print(sum([k for i in text for k, v in eng.items() if i in v]))
+	print(sum([k for i in text for k, v in eng.items() if i in v]), " очков")
